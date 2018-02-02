7135e35ac3fdf24a35dd06b91ecf75937c0979d9
Gajanan Mirashe <gajananmirashe10@gmail.com>
2018-02-03 02:12:34 +0530

    Modified Test Class

--- src/test/java/com/fdc/automationDemo/browserLaunch/launchingBrowser.java
+++ src/test/java/com/fdc/automationDemo/browserLaunch/launchingBrowser.java
@@ -1,7 +1,21 @@
 package com.fdc.automationDemo.browserLaunch;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
 public class launchingBrowser {
 	
 	//Launching Browser methods - 
+	@Test
+	public void getBrowser(){
+	
+		System.setProperty("webdriver.chrome.driver","F:\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.google.com");
+		driver.quit();
 
+	}
+	
 }
