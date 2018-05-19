64eb9603c102f72ea2319f23ca51ce29df27b692
Gajanan Mirashe <gajananmirashe10@gmail.com>
2018-05-20 01:54:50 +0530

    removed unwanted imports

--- src/test/java/com/fdc/automationDemo/browserLaunch/launchingBrowser.java
+++ src/test/java/com/fdc/automationDemo/browserLaunch/launchingBrowser.java
@@ -3,21 +3,19 @@ package com.fdc.automationDemo.browserLaunch;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
-import org.testng.asserts.SoftAssert;
 
 public class launchingBrowser {
-	
-	//Launching Browser methods - 
+
+	// Launching Browser methods -
 	@Test
-	public void getBrowser(){
-	
-		System.setProperty("webdriver.chrome.driver","F:\\chromedriver_win32\\chromedriver.exe");
+	public void getBrowser() {
+
+		System.setProperty("webdriver.chrome.driver", "F:\\chromedriver_win32\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://www.google.com");
 		driver.quit();
-		
 
 	}
-	
+
 }
